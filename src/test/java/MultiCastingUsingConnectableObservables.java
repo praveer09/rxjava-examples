57b5e55ed1c053e5c4d7aa74012662153fa739e5
Praveer Gupta <praveerg@inpraveerg.local>
2016-08-27 16:10:59 +0530

    Add multi-casting tests

---
+++ src/test/java/MultiCastingUsingConnectableObservables.java
@@ -0,0 +1,75 @@
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rx.observables.ConnectableObservable;
+import rx.observers.TestSubscriber;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static rx.Observable.defer;
+import static rx.Observable.just;
+
+public class MultiCastingUsingConnectableObservables {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Test
+    public void doesNotStartEmittingEventsTillConnectIsCalled() throws Exception {
+        TestSubscriber<String> testSubscriber = TestSubscriber.create();
+        ConnectableObservable<String> observable = just("test")
+                .doOnNext(logger::info)
+                .publish();
+
+        observable.subscribe(testSubscriber);
+
+        testSubscriber.assertNoValues();
+
+        observable.connect();
+        testSubscriber.awaitTerminalEvent();
+        testSubscriber.assertValue("test");
+    }
+
+    @Test
+    public void multiCastsSameEventToMultipleSubscribers() throws Exception {
+        TestSubscriber<Instant> subscriber1 = TestSubscriber.create();
+        TestSubscriber<Instant> subscriber2 = TestSubscriber.create();
+
+        ConnectableObservable<Instant> observable = defer(() -> just(Instant.now()))
+                .doOnNext(i -> logger.info(i.toString()))
+                .publish();
+
+        observable.subscribe(subscriber1);
+        observable.subscribe(subscriber2);
+
+        observable.connect();
+
+        subscriber1.awaitTerminalEvent();
+        subscriber2.awaitTerminalEvent();
+
+        assertThat(subscriber1.getOnNextEvents())
+                .containsExactlyElementsOf(subscriber2.getOnNextEvents());
+    }
+
+    @Test
+    public void connectHasToBeCalledAgainToRestartAfterCompleted() throws Exception {
+        TestSubscriber<Instant> subscriber1 = TestSubscriber.create();
+        TestSubscriber<Instant> subscriber2 = TestSubscriber.create();
+
+        ConnectableObservable<Instant> observable = defer(() -> just(Instant.now()))
+                .doOnNext(i -> logger.info(i.toString()))
+                .publish();
+
+        observable.subscribe(subscriber1);
+        observable.connect();
+        subscriber1.awaitTerminalEvent();
+        subscriber1.assertValueCount(1);
+
+        observable.subscribe(subscriber2);
+        observable.connect();
+        subscriber2.awaitTerminalEvent();
+        subscriber2.assertValueCount(1);
+
+        assertThat(subscriber1.getOnNextEvents().get(0))
+                .isNotEqualTo(subscriber2.getOnNextEvents().get(0));
+    }
+}
